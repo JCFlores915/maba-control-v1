f502ba0c55afcdd915cdb23a907c6dd913e3eec0
JCFlores915 <mauricio@build-it.cl>
2025-03-13 18:46:17 -0600

    first

---
+++ src/routes/MainRoutes.tsx
@@ -0,0 +1,43 @@
+import { lazy } from 'react';
+
+// project import
+import MainLayout from 'layout/MainLayout';
+import Loadable from 'components/Loadable';
+import AuthGuard from 'utils/route-guard/AuthGuard';
+
+// render - dashboard
+const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/default')));
+const DashboardAnalytics = Loadable(lazy(() => import('pages/dashboard/analytics')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  children: [
+    {
+      path: '/',
+      element: (
+        <AuthGuard>
+          <MainLayout />
+        </AuthGuard>
+      ),
+      children: [
+        {
+          path: 'dashboard',
+          children: [
+            {
+              path: 'default',
+              element: <DashboardDefault />
+            },
+            {
+              path: 'analytics',
+              element: <DashboardAnalytics />
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+export default MainRoutes;
